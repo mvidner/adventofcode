4871a3376722176bf0476419054404e9944118a5
Martin Vidner <martin@vidner.net>
2016-02-16 22:32:09 +0100

    Solved #4.

---
+++ 04/solution.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require "digest"
+
+$input = "bgvyzdsv"
+
+def md5(s)
+  Digest::MD5.hexdigest(s)
+end
+
+def task_md5(key, number)
+  md5("#{key}#{number}")
+end
+
+def good_number?(number)
+  task_md5($input, number)[0..4] == "00000"
+end
+
+i = 0
+while !good_number?(i)
+  i += 1
+end
+puts i
+
