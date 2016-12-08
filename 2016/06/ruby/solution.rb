5e4db5fb89c8e704fdea5f78930facbcdd873cbf
Martin Vidner <martin@vidner.net>
2016-12-08 19:46:26 +0100

    Solved 2016/6 part one

---
+++ 2016/06/ruby/solution.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+messages = File.readlines("input.txt").map(&:chomp)
+
+histograms = Array.new(messages.first.size) { Hash.new }
+
+messages.each do |m|
+  m.each_char.each_with_index do |c, i|
+    histogram = histograms[i]
+    histogram[c] ||= 0
+    histogram[c] += 1
+  end
+end
+
+puts "Solution:"
+histograms.each do |h|
+  print h.to_a.max_by {|_char, count| count}.first
+end
+puts
