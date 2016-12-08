caebe85d10cc3a83d7d4856448e8e3b77e61668c
Martin Vidner <martin@vidner.net>
2016-12-08 07:20:27 +0100

    First try of solving 2016/8

---
+++ 2016/08/ruby/solution.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+class Display
+  OFF = 0
+  ON  = 1
+  def initialize(x, y)
+    @w = x
+    @h = y
+    @pix = Array.new(@h) { Array.new(@w) { OFF } }
+  end
+
+  def count_pixels
+    @pix.map { |r| r.count { |c| c == ON } }.reduce(0, :+)
+  end
+
+  def perform(instr)
+    case instr
+    when /rect (\d+)x(\d+)/
+      perform_rect($1.to_i, $2.to_i)
+    when /rotate row y=(\d+) by (\d+)/
+      perform_rrow($1.to_i, $2.to_i)
+    when /rotate column x=(\d+) by (\d+)/
+      perform_rcol($1.to_i, $2.to_i)
+    else
+      raise
+    end
+  end
+
+  private
+
+  def perform_rect(w, h)
+    (0 .. h-1).each do |y|
+      @pix[y][0, w - 1] = Array.new(w) { ON }
+    end
+  end
+
+  def perform_rrow(y, count)
+    @pix = functional_rot(@pix, @w, y, count)
+  end
+
+  def perform_rcol(x, count)
+    @pix = functional_rot(@pix.transpose, @h, x, count).transpose
+  end
+
+  def functional_rot(pixels, width, row_idx, count)
+    row = pixels[row_idx]
+    newrow = row[count, -1] + row[0, width - count]
+    row.replace(newrow)
+    pixels
+  end
+end
+
+instructions = File.readlines("input.txt")
+
+d1 = Display.new(50, 6)
+instructions.each { |i| d1.perform(i) }
+puts "Solution:"
+puts d1.count_pixels
