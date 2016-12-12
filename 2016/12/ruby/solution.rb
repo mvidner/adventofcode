8e21245d011d1c3fa2c5e861ebd7e58ba1940060
Martin Vidner <martin@vidner.net>
2016-12-12 07:15:00 +0100

    Solved 2016/12 part one (# and two)

--- 2016/12/ruby/solution.rb
+++ 2016/12/ruby/solution.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
-instrs = File.readlines("input.txt")
+instrs = File.readlines("input.txt").map(&:chomp)
 
 timer = 0
 ip = 0
 regs = {}
 regs[:a] = regs[:b] = regs[:c] = regs[:d] = 0
+# regs[:c] = 1
 
 loop do
   instr = instrs[ip]
   ip += 1
+  timer += 1
+  puts "#{ip}: #{regs}; #{timer}" if timer % 1000000 == 0
   break if instr.nil?
 
   case instr
@@ -28,8 +31,14 @@ loop do
   when /jnz ([a-d]) ([0-9-]+)/
     r = $1.to_sym
     if regs[r] != 0
-      ip += $2.to_i
+      ip += $2.to_i - 1
     end
+  when /jnz ([0-9]+) ([0-9-]+)/
+    if $1.to_i != 0
+      ip += $2.to_i - 1
+    end
+  else
+    raise puts "#{instr}: #{ip}: #{regs}"
   end
 end
 
