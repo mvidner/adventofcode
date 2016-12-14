cb5e5cfedebe4b9514e0aec018bb0489852beece
Martin Vidner <martin@vidner.net>
2016-12-14 21:09:49 +0100

    The wrong caching for part two

--- 2016/14/ruby/solution.rb
+++ 2016/14/ruby/solution.rb
@@ -11,12 +11,23 @@ def cached_md5(s)
   $md5_cache[s] ||= md5(s)
 end
 
+def stretched_md5(s)
+  2017.times do
+    s = cached_md5(s)
+  end
+  s
+end
+
 def hash(salt, i)
   md5("#{salt}#{i}")
 end
 
 def cached_hash(salt, i)
   cached_md5("#{salt}#{i}")
+end
+
+def stretched_hash(salt, i)
+  stretched_md5("#{salt}#{i}")
 end
 
 # return nil or the one character repeated
@@ -26,13 +37,14 @@ def has_triple?(s)
   end
 end
 
-def solve(salt)
+def solve(salt, hash_name = :hash)
   keys_found = 0
   i = 0
   loop do
-    h = cached_hash(salt, i)
+    h = send(hash_name, salt, i)
     if (c = has_triple?(h))
-      if ((i + 1)..(i + 1000)).any? { |j| cached_hash(salt, j).include?(c * 5) }
+      r = (i + 1)..(i + 1000)
+      if r.any? { |j| send(hash_name, salt, j).include?(c * 5) }
         keys_found += 1
       end
     end
@@ -44,5 +56,20 @@ def solve(salt)
   puts i
 end
 
-solve("abc")
-solve("qzyelonm")
+def time_it
+  t0 = Time.now
+  yield
+  t1 = Time.now
+  printf("It took %.4g seconds.\n", t1 - t0)
+end
+
+sample = "abc"
+input  = "qzyelonm"
+time_it { solve(sample, :hash) }
+time_it { solve(input,  :hash) }
+
+time_it { solve(sample, :cached_hash) }
+time_it { solve(input,  :cached_hash) }
+
+time_it { solve(sample, :stretched_hash) }
+time_it { solve(input,  :stretched_hash) }
