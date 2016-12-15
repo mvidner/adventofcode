9f6ac9ab5b6d544c6fc035b254d32a83901fedf8
Martin Vidner <martin@vidner.net>
2016-12-15 20:54:58 +0100

    Solved 2016/15 Timing is Everything
    It took 630ms

--- 2016/15/ruby/solution.rb
+++ 2016/15/ruby/solution.rb
@@ -1,1 +1,22 @@
 #!/usr/bin/env ruby
+
+def satisfied_a(t)
+  # optimized this by hand on paper, which turned to take much more time
+  t % 13 == 2 &&
+    t % 17 == 0 &&
+    t % 19 == 18 &&
+    t % 7 == 2 &&
+    t % 5 == 0 &&
+    t % 3 == 2
+end
+
+def satisfied_b(t)
+  satisfied_a(t) &&
+    (t + 7 + 0) % 11 == 0
+end
+
+puts "Part A:"
+puts 1.upto(Float::INFINITY).find {|t| satisfied_a(t)}
+
+puts "Part B:"
+puts 1.upto(Float::INFINITY).find {|t| satisfied_b(t)}
