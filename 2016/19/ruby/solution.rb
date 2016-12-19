7bc251331ff5810bb1c29bd9e7bbfc83dc7fcde4
Martin Vidner <martin@vidner.net>
2016-12-19 22:30:13 +0100

    slowly solving part two

--- 2016/19/ruby/solution.rb
+++ 2016/19/ruby/solution.rb
@@ -25,8 +25,42 @@ def next_elf(elves, i)
   i
 end
 
+def next_elf_across(elves, i, left)
+  (left / 2).times do
+    i = next_elf(elves, i)
+  end
+  i
+end
+
+def white_elephant_party_two(n)
+  # this time, elves is the array of the remaining elves, with their
+  # original numbers as values
+  elves = 1.upto(n).to_a
+
+  i = 0
+  while elves.size > 1
+    n = elves.size
+    puts n if 0 == n % 10000
+    i = i % n
+    victim = (i + (n / 2)) % n
+#    puts "@#{i}'s turn, the victim is @#{victim}"
+#    p elves
+    elves.delete_at(victim)
+    i += 1 if victim > i
+  end
+
+  elves.first
+end
+
+
 puts "Example:"
 puts white_elephant_party(5)
 
 puts "Solution:"
-puts white_elephant_party(3014603)
+#puts white_elephant_party(3014603)
+
+puts "Example part two:"
+puts white_elephant_party_two(5)
+
+puts "Solution part two:"
+puts white_elephant_party_two(3014603)
