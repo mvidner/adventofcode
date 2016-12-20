6d0e570bbe657966c765a26207fe778cd60afbd7
Martin Vidner <martin@vidner.net>
2016-12-21 00:14:04 +0100

    How not to do it in 58 minutes

--- 2016/20/ruby/solution.rb
+++ 2016/20/ruby/solution.rb
@@ -1,49 +1,16 @@
 #!/usr/bin/env ruby
-require "set"
+require "bitarray"
 
-all = (0 ... (2 ** 32))
+allowed = BitArray.new(2 ** 32)
 
-rules = File.readlines("input.txt").map do |line|
+File.readlines("input.txt").each_with_index do |line, i|
   line =~ /(\d+)-(\d+)/
-  low = $1
-  high = $2
+  low = $1.to_i
+  high = $2.to_i
   raise "Invalid range input" if high < low
-  (low .. high)
+  puts "#{i}: #{high-low} #{line}"
+  (low .. high).each { |a| allowed[a] = 0 }
 end
 
-# a non-overlapping set of Ranges
-class RangeSet
-  # it starts with a single Range
-  def initialize(range)
-    @set = Set.new
-    @set << range
-  end
-
-  def subtract(range)
-    b = range.be
-  end
-end
-
-# extend it
-class Range
-  def subtract(other)
-    raise ArgumentError unless other.is_a? Range
-    raise ArgumentError unless exclude_end? && other.exclude_end?
-
-    if other.begin < self.begin
-      if other.end < self.begin
-        self
-      elsif other.end < self.end # <= ?
-
-    
-  end
-
-  def to_open
-    if exclude_end?
-      self
-    else
-      (self.begin ... self.end.succ)
-    end
-  end
-
-end
+puts "Solution:"
+puts (0 ... 2 ** 32).find { |i| allowed[i] == 1 }
