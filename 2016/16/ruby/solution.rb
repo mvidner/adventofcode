3de33a312258119401e6444aec0d4af0f81c872b
Martin Vidner <martin@vidner.net>
2016-12-22 18:27:04 +0100

    SOlved 2016/16

--- 2016/16/ruby/solution.rb
+++ 2016/16/ruby/solution.rb
@@ -1,1 +1,29 @@
 #!/usr/bin/env ruby
+
+DISK_SIZE = 272
+input = "10111100110001111"
+
+def dragon_step(s)
+  s + "0" + s.reverse.tr("01", "10")
+end
+
+def checksum(s)
+  if s.size.odd?
+    s
+  else
+    s = s.each_char.each_slice(2).map do |a, b|
+      (a == b) ? "1" : "0"
+    end.join("")
+    checksum(s)
+  end
+end
+
+puts "Solution:"
+
+disk = input
+while disk.size < DISK_SIZE
+  disk = dragon_step(disk)
+end
+disk = disk[0, DISK_SIZE]
+
+puts checksum(disk)
