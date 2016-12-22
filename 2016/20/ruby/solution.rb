b440faff1a3bd62a1b50a7b9d7d539df39010925
Martin Vidner <martin@vidner.net>
2016-12-22 17:57:54 +0100

    Solved 2016/20 part two.

--- 2016/20/ruby/solution.rb
+++ 2016/20/ruby/solution.rb
@@ -7,10 +7,12 @@ blocked = File.readlines("input.txt").map do |line|
   raise "Invalid range input" if high < low
   [low, high]
 end
+blocked.sort!
+blocked << [2 ** 32, 2 ** 32]   # sentinel value
 
 puts "Solution:"
 first_allowed = 0
-blocked.sort.each do |low, high|
+blocked.each do |low, high|
   if high < first_allowed
     next
   elsif (low .. high).member?(first_allowed)
@@ -20,3 +22,19 @@ blocked.sort.each do |low, high|
   end
 end
 puts first_allowed
+
+puts "Part two:"
+first_allowed = 0
+allowed_count = 0
+blocked.each do |low, high|
+  # invariant: allowed_count <= limit <= low <= high
+  if high < first_allowed
+    next
+  elsif (low .. high).member?(first_allowed)
+    first_allowed = high + 1
+  else # first_allowed < low
+    allowed_count += low - first_allowed
+    first_allowed = high + 1
+  end
+end
+puts allowed_count
