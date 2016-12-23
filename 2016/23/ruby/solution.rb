76469ab963079b7fbc5d9535ae6048817f902435
Martin Vidner <martin@vidner.net>
2016-12-23 10:42:28 +0100

    2016/23: registers Hash -> Array

--- 2016/23/ruby/solution.rb
+++ 2016/23/ruby/solution.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/env ruby
 
-def decode(arg, regs)
+A_ORD = "a".ord
+
+# @param regarr [Array] *a* at 0, *d* at 3
+def decode(arg, regarr)
   if ("a" .. "d").cover?(arg[0])
-    regs[arg.to_sym]
+    regarr[arg.ord - A_ORD]
   else
     arg.to_i
   end
 end
 
-# @param regs [Hash] initial register values
-# @return [Hash] final register values
-def run(instrs, regs)
-  regs = regs.dup               # do not mutate the argument
+# @param regarr [Array] initial register values
+# @return [Array] final register values
+def run(instrs, regarr)
+  regarr = regarr.dup               # do not mutate the argument
   instrs = instrs.dup           # do not mutate the original instructions either
   timer = 0
   ip = 0
@@ -20,33 +23,33 @@ def run(instrs, regs)
     instr = instrs[ip]
     ip += 1
     timer += 1
-    puts "#{ip}: #{regs}; #{timer}" if 0 == timer % 1000000
+    puts "#{ip}: #{regarr}; #{timer}" if 0 == timer % 1000000
     break if instr.nil?
     case instr
     when /inc ([a-d])/
-      r = $1.to_sym
-      regs[r] += 1
+      r = $1.ord - A_ORD
+      regarr[r] += 1
     when /dec ([a-d])/
-      r = $1.to_sym
-      regs[r] -= 1
+      r = $1.ord - A_ORD
+      regarr[r] -= 1
 
     when /cpy ([a-d0-9-]+) ([a-d])/
-      val  = decode($1, regs)
-      dest = $2.to_sym
-      regs[dest] = val
+      val  = decode($1, regarr)
+      dest = $2.ord - A_ORD
+      regarr[dest] = val
 
     when /cpy ([a-d0-9-]+) ([0-9-]+)/
       # invalid instruction, skip it
 
     when /jnz ([a-d0-9-]+) ([a-d0-9-]+)/
-      cond = decode($1, regs)
-      displacement = decode($2, regs)
+      cond = decode($1, regarr)
+      displacement = decode($2, regarr)
       if cond != 0
         ip += displacement - 1
       end
 
     when /tgl ([a-d])/
-      displacement = regs[$1.to_sym]
+      displacement = regarr[$1.ord - A_ORD]
       addr = ip - 1 + displacement
       next unless (0...instrs.size).cover?(addr)
       opcode = {
@@ -56,11 +59,11 @@ def run(instrs, regs)
       # make a new string, do not mutate the original string
       instrs[addr] = opcode + instrs[addr][3..-1]
     else
-      raise "#{instr}: #{ip}: #{regs}"
+      raise "#{instr}: #{ip}: #{regarr}"
     end
   end
 
-  regs
+  regarr
 end
 
 def time_it
@@ -72,22 +75,19 @@ end
 
 instrs = File.readlines("input.txt").map(&:chomp)
 
-regs = {}
-regs[:a] = regs[:b] = regs[:c] = regs[:d] = 0
-regs[:a] = 7
+regarr = [7, 0, 0, 0]
 
 p instrs
 time_it do
   puts "Solution to part A:"
-  aregs = run(instrs, regs)
-  puts aregs[:a]
+  aregs = run(instrs, regarr)
+  puts aregs[0]
 end
 
 puts
 time_it do
-  regs[:a] = 12
-p instrs, regs
+  regarr[0] = 12
   puts "Solution to part B:"
-  bregs = run(instrs, regs)
-  puts bregs[:a]
+  bregs = run(instrs, regarr)
+  puts bregs[0]
 end
