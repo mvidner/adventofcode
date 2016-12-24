9918369a53010542d6c3c4144531f430c4e6c210
Martin Vidner <martin@vidner.net>
2016-12-24 08:18:24 +0100

    Cleaned up 2016/24

--- 2016/24/ruby/solution.rb
+++ 2016/24/ruby/solution.rb
@@ -2,13 +2,8 @@
 require "set"
 require "pp"
 
-$timer = 0
-$timer_step = 1
-
-
 class Map
   attr_accessor :rows
-  attr_reader :reached
   attr_reader :numbers
 
   def self.from_file(filename)
@@ -34,20 +29,6 @@ class Map
     @numbers = arr
   end
 
-  # return index of manhattan-closest number from @x @y
-  def closest
-    distances = numbers.map do |pair|
-      if pair.nil?
-        Float::INFINITY
-      else
-        x, y = *pair
-        (x - @x).abs + (y - @y).abs
-      end
-    end
-    min = distances.min
-    distances.index(min)
-  end
-
   def distances
     return @distances if @distances
     @distances = []
@@ -57,19 +38,10 @@ class Map
     @distances
   end
 
+  # return [length of Hamiltonian path, length of Hamiltonian cycle]
   def visit_all
+    puts "Distance matrix:"
     pp distances
-    min_path = (1 ... @numbers.size).to_a.permutation.map do |order|
-      steps = 0
-      current = 0
-      order.each do |nxt|
-        steps += distances[current][nxt]
-        current = nxt
-      end
-      # return the path length
-      steps
-    end.tap {|p| pp p}.min
-    puts "Minimal path #{min_path}"
 
     min_path = (1 ... @numbers.size).to_a.permutation.map do |order|
       steps = 0
@@ -79,9 +51,8 @@ class Map
         current = nxt
       end
       # return the path length
-      steps + distances[current][0]
-    end.tap {|p| pp p}.min
-    puts "Minimal return path #{min_path}"
+      [steps, steps + distances[current][0]]
+    end.min
 
   end
 
@@ -91,19 +62,6 @@ class Map
       r.dup
     end
     copy
-  end
-
-  # @return [self] self if goal reached, otherwise nil
-  def go(x, y)
-    @x = x
-    @y = y
-    trace
-    found = check!
-    return found unless found.nil?
-
-    @steps += 1
-    @rows[y][x] = "O"
-    nil
   end
 
   def print_map
@@ -117,68 +75,6 @@ class Map
       end
       puts
     end
-  end
-
-  def sgn(a)
-    return  1 if a > 0
-    return -1 if a < 0
-    0
-  end
-
-  def check!
-    if @x == @fx && @y == @fy
-      puts "GOAL, in #{@steps} steps"
-      self
-    else
-      nil
-    end
-  end
-
-  def best_steps
-    dx = @fx - @x
-    dy = @fy - @y
-    sx = sgn(dx)
-    sy = sgn(dy)
-    if dx.abs > dy.abs
-      sy = 1 if sy == 0
-      ss = [[sx, 0], [0, sy], [0, -sy], [-sx, 0]]
-    else
-      sx = 1 if sx == 0
-      ss = [[0, sy], [sx, 0], [-sx, 0], [0, -sy]]
-    end
-    ss
-  end
-
-  # @return [self] self if goal reached, otherwise nil
-  def step(x, y)
-    found = go(x, y)
-    return found unless found.nil?
-
-    bs = best_steps
-    bs.each_with_index do |d, i|
-      dx, dy = d
-      ny = @y + dy
-      nx = @x + dx
-      if !within_board?(nx, ny)
-        print "@"
-        next
-      end
-
-      if @rows[ny][nx] == "."
-        found = dup.step(nx, ny)
-        return found unless found.nil?
-      end
-    end
-    # we have exhausted all steps
-    nil
-  end
-
-  def trace
-    $timer += 1
-    return unless 0 == $timer % $timer_step
-    puts
-    print_map
-    puts $timer
   end
 
   def within_board?(x, y)
@@ -235,6 +131,13 @@ class Map
   end
 end
 
+def solve(map)
+  map.print_map
+  path, cycle = map.visit_all
+  puts "Minimal path #{path}"
+  puts "Minimal return path #{cycle}"
+end
+
 puts "Sample:"
 sample_str = <<EOS
 ###########
@@ -244,36 +147,8 @@ sample_str = <<EOS
 ###########
 EOS
 sample = Map.new(sample_str.split /\n/)
-sample.print_map
-p sample.numbers
-sample.visit_all
+solve(sample)
 
-puts "Part one:"
+puts "Puzzle:"
 map = Map.from_file("input.txt")
-puts "Numbers:"
-p map.numbers
-map.visit_all
-
-exit
-
-puts "Part A:"
-test = Map.new(10, 8, 7, 4, 10)
-# test.step(1, 1)
-
-part_a = Map.new(50, 50, 31, 39, 1352)
-found = part_a.step(1, 1)
-found.print_map
-
-m = Map.new(15, 15, 10, 8, 1352)
-# m.step(1, 1)
-
-puts "Part B:"
-limit = 50
-part_b0 = Map.new(limit+2, limit+2, nil, nil, 1352)
-
-limit.upto(limit).each do |lim|
-  part_b = part_b0.dup
-  reached = part_b.reach(1, 1, lim)
-  part_b.print_map
-  puts "In #{lim} steps we can reach #{reached.reached} cells."
-end
+solve(map)
