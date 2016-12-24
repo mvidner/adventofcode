62404f54e3b8560451741d57fe9f3ea4e61274fe
Martin Vidner <martin@vidner.net>
2016-12-24 11:05:17 +0100

    Optimized multiplication for 2016/23

--- 2016/23/ruby/solution.rb
+++ 2016/23/ruby/solution.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require "pp"
+
 # instruction internal representation is a tuple
 # (opcode, arg1_is_register, arg1_num, arg2_is_register, arg2_num)
 #
@@ -32,6 +34,69 @@ def parse(string_instr)
   end
 end
 
+def dump(label, instrs)
+  puts label
+  return
+  pp instrs
+end
+
+# may mutate instrs
+def post_optimize_jnz!(instrs, ip)
+  opcode, _cond_isreg, _cond_num, displ_isreg, displ_num = * instrs[ip]
+  raise unless opcode == :jnz
+
+  # inc X      or   dec Y
+  # dec Y           inc X
+  # jnz Y, -2       jnz Y, -2
+  #            ->
+  # add Y, X
+  # cpy_ 0, Y
+  # nop
+  if !displ_isreg && displ_num == -2
+    dec_opcode, _, dec_reg, _, _ = * instrs[ip - 1]
+    inc_opcode, _, inc_reg, _, _ = * instrs[ip - 2]
+    if dec_opcode == :inc && inc_opcode == :dec
+      dec_opcode, inc_opcode = :dec, :inc
+      dec_reg, inc_reg = inc_reg, dec_reg
+    end
+    if dec_opcode == :dec && inc_opcode == :inc
+      dump "BEFORE", instrs
+      instrs[ip - 2] = [:add,  true, dec_reg, true, inc_reg]
+      instrs[ip - 1] = [:cpy_, false, 0,      true, dec_reg]
+      instrs[ip    ] = [:nop,  nil, nil, nil, nil]
+      dump "AFTER", instrs
+    end
+  end
+
+  # cpy X, Y
+  # add Y, Z
+  # (any, actually cpy 0, Y)
+  # (any, actually nop)
+  # dec W
+  # jnz W, -5
+  #   ->
+  # cpy X, Y
+  # mul Y, W
+  # (keep)
+  # (keep)
+  # add W, Z
+  # cpy_ 0, W
+  if !displ_isreg && displ_num == -5
+    cpy_opcode, _, src0_reg, _, dst0_reg = * instrs[ip - 5]
+    add_opcode, _, src_reg,  _, dst_reg  = * instrs[ip - 4]
+    dec_opcode, _, dec_reg,  _, _        = * instrs[ip - 1]
+    if cpy_opcode == :cpy && dec_opcode == :dec && add_opcode == :add &&
+        dst0_reg == src_reg
+      dump "BEFORE MUL", instrs
+      instrs[ip - 4] = [:mul,  true, src_reg, true, dec_reg]
+      instrs[ip - 1] = [:add,  true, dec_reg, true, dst_reg]
+      instrs[ip    ] = [:cpy_, false, 0,      true, dec_reg]
+      dump "AFTER MUL", instrs
+    end
+  end
+
+end
+
 # @param regarr [Array] initial register values
 # @return [Array] final register values
 def run(instrs, regarr)
@@ -42,11 +107,14 @@ def run(instrs, regarr)
 
   loop do
     instr = instrs[ip]
-    ip += 1
+    puts "#{ip}: #{regarr}; #{timer}" if 0 == timer % 1#000000
+
+    next_ip = ip + 1
     timer += 1
-    puts "#{ip}: #{regarr}; #{timer}" if 0 == timer % 1000000
     break if instr.nil?
     case instr[0]
+    when :nop
+      # no operation
     when :inc
       r = instr[2]
       regarr[r] += 1
@@ -54,7 +122,7 @@ def run(instrs, regarr)
       r = instr[2]
       regarr[r] -= 1
 
-    when :cpy
+    when :cpy, :cpy_
       _opcode, src_isreg, src_num, dest_isreg, dest_num = * instr
       if dest_isreg
         regarr[dest_num] = src_isreg ? regarr[src_num] : src_num
@@ -62,44 +130,52 @@ def run(instrs, regarr)
         # invalid instruction, skip it
       end
 
-    when :jnz
-      _opcode, cond_isreg, cond_num, displ_isreg, displ_num = * instr
-      if cond_isreg
-        if regarr[cond_num] != 0
-          displacement = displ_isreg ? regarr[displ_num] : displ_num
-          if displacement == -2 && instrs[ip - 3][0] == :inc && instrs[ip - 2][0] == :dec
-            dstreg = instrs[ip - 3][2]
-            srcreg = instrs[ip - 2][2]
-            regarr[dstreg] += regarr[srcreg]
-            timer += regarr[srcreg] * 3 # approximately
-            regarr[srcreg] = 0
-            # ip correctly points after the loop
-          else
-            ip += displacement - 1
-          end
-        end
+    when :add
+      _opcode, src_isreg, src_num, dest_isreg, dest_num = * instr
+      if dest_isreg
+        regarr[dest_num] += src_isreg ? regarr[src_num] : src_num
       else
-        if cond_num != 0
-          displacement = displ_isreg ? regarr[displ_num] : displ_num
-          ip += displacement - 1
-        end
+        # invalid instruction, skip it
       end
 
+    when :mul
+      _opcode, src_isreg, src_num, dest_isreg, dest_num = * instr
+      if dest_isreg
+        regarr[dest_num] *= src_isreg ? regarr[src_num] : src_num
+      else
+        # invalid instruction, skip it
+      end
+
+    when :jnz
+      _opcode, cond_isreg, cond_num, displ_isreg, displ_num = * instr
+      cond = cond_isreg ? regarr[cond_num] : cond_num
+      if cond != 0
+        displacement = displ_isreg ? regarr[displ_num] : displ_num
+        next_ip = ip + displacement
+      end
+      post_optimize_jnz!(instrs, ip)
+
     when :tgl
-      addr = ip - 1 + (instr[1] ? regarr[instr[2]] : instr[2])
-      next unless (0...instrs.size).cover?(addr)
-      opcode = {
-        inc: :dec,
-        dec: :inc,
-        tgl: :inc,
-        cpy: :jnz,
-        jnz: :cpy
-      }.fetch(instrs[addr][0])
-      # make a new instruction
-      instrs[addr] = [opcode] + instrs[addr][1..-1]
+      addr = ip + (instr[1] ? regarr[instr[2]] : instr[2])
+      if (0...instrs.size).cover?(addr)
+        # instructions produced by the optimizer cannot be toggled
+        # and attempting to do so will crash the machine
+        opcode = {
+          inc: :dec,
+          dec: :inc,
+          tgl: :inc,
+          cpy: :jnz,
+          jnz: :cpy
+        }.fetch(instrs[addr][0])
+        # make a new instruction
+        instrs[addr] = [opcode] + instrs[addr][1..-1]
+        dump "TOGGLED", instrs
+      end
     else
       raise "Unknown instruction #{instr}"
     end
+
+    ip = next_ip
   end
 
   regarr
