3380855684fac57a48fcb92e08a0d02d3ee16457
Martin Vidner <martin@vidner.net>
2017-12-02 09:13:33 +0100

    2017/02 Corruption Checksum: solved

--- 2017/02/ruby/solution.rb
+++ 2017/02/ruby/solution.rb
@@ -1,1 +1,30 @@
 #!/usr/bin/env ruby
+lines = File.readlines("input.txt")
+
+diffs = lines.map do |line|
+  nums = line.split.map(&:to_i)
+  nums.max - nums.min
+end
+
+checksum = diffs.reduce(0, :+)
+puts checksum
+
+# a divides b
+def divides?(a, b)
+  return false if a == 0
+  b % a == 0 && b / a != 1
+end
+  
+ratios = lines.map do |line|
+  nums = line.split.map(&:to_i)
+  ratio = nil
+  (0 .. nums.size - 1).each do |a|
+    (0 .. nums.size - 1).each do |b|
+      ratio = nums[b] / nums[a] if divides?(nums[a], nums[b])
+    end
+  end
+  ratio
+end
+
+checksum2 = ratios.reduce(0, :+)
+puts checksum2
