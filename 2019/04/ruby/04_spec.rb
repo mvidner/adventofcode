4531cbc96ad889497d19a55ea7a0606a1062c9f3
Martin Vidner <martin@vidner.net>
2019-12-04 23:40:28 +0100

    Part 2 of 2019/04

--- 2019/04/ruby/04_spec.rb
+++ 2019/04/ruby/04_spec.rb
@@ -10,3 +10,14 @@ describe "#valid_try?" do
     expect(valid_try?(123789)).to eq false
   end
 end
+
+describe "#more_valid_try?" do
+  it "is true for good samples" do
+    expect(more_valid_try?(112233)).to eq true
+    expect(more_valid_try?(111122)).to eq true
+  end
+
+  it "is false for bad samples" do
+    expect(more_valid_try?(123444)).to eq false
+  end
+end
--- 2019/04/ruby/solution.rb
+++ 2019/04/ruby/solution.rb
@@ -11,13 +11,25 @@ def valid_try?(n)
   true
 end
 
+DIGIT_RUNS = /0+|1+|2+|3+|4+|5+|6+|7+|8+|9+/
+def more_valid_try?(n)
+  runs = n.to_s.scan DIGIT_RUNS
+  runs.any? { |r| r.size == 2 }
+end
+
 if $0 == __FILE__
   input = File.read("input.txt")
+  min, max = input.split("-").map(&:to_i)
 
   puts "Part 1"
-  min, max = input.split("-").map(&:to_i)
   part1 = (min..max).count do |i|
     valid_try?(i)
   end
   puts part1
+
+  puts "Part 2"
+  part2 = (min..max).count do |i|
+    valid_try?(i) && more_valid_try?(i)
+  end
+  puts part2
 end
