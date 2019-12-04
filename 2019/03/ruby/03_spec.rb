920b64abf3a4c939701076103f98c5737250606c
Martin Vidner <martin@vidner.net>
2019-12-04 23:01:30 +0100

    Part 2 of 2019/03

--- 2019/03/ruby/03_spec.rb
+++ 2019/03/ruby/03_spec.rb
@@ -1,15 +1,26 @@
 require_relative "solution"
 
+sample1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" \
+          "U62,R66,U55,R34,D71,R55,D58,R83"
+sample2 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" \
+          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+
 describe "#crossing_distance" do
   it "works for sample 1" do
-    input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" \
-            "U62,R66,U55,R34,D71,R55,D58,R83"
-    expect(crossing_distance(input)).to eq(159)
+    expect(crossing_distance(sample1)).to eq(159)
   end
 
   it "works for sample 2" do
-    input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" \
-            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-    expect(crossing_distance(input)).to eq(135)
+    expect(crossing_distance(sample2)).to eq(135)
+  end
+end
+
+describe "#crossing_steps" do
+  it "works for sample 1" do
+    expect(crossing_steps(sample1)).to eq(610)
+  end
+
+  it "works for sample 2" do
+    expect(crossing_steps(sample2)).to eq(410)
   end
 end
--- 2019/03/ruby/solution.rb
+++ 2019/03/ruby/solution.rb
@@ -6,7 +6,7 @@ def crossing_distance(directions_s)
   wire1 = plot(lines[0])
   wire2 = plot(lines[1])
 
-  crossings = wire1.intersection(wire2)
+  crossings = Set.new(wire1.keys).intersection(Set.new(wire2.keys))
   crossing_distances = crossings.map do |point|
     point[0].abs + point[1].abs
   end
@@ -14,12 +14,28 @@ def crossing_distance(directions_s)
   crossing_distances.sort[1]
 end
 
-# We represent a wire by a {Set} of grid points which it passes through.
-# A point is simply a pair of [x, y] coordinates
+def crossing_steps(directions_s)
+  lines = directions_s.split "\n"
+  wire1 = plot(lines[0])
+  wire2 = plot(lines[1])
+
+  crossings = Set.new(wire1.keys).intersection(Set.new(wire2.keys))
+  crossing_steps = crossings.map do |point|
+    wire1[point] + wire2[point]
+  end
+  # the 1st element [0] is [0,0] but we want the nontrivial solution
+  crossing_steps.sort[1]
+end
+
+# We represent a wire by a {Hash}:
+# the keys are the grid points which it passes through.
+# (A point is simply a pair of [x, y] coordinates)
+# The values are the number of steps required to reach that point
 def plot(dirs)
+  steps = 0
   pos = [0, 0]
-  wire = Set.new
-  wire << pos.dup
+  wire = {}
+  wire[pos.dup] ||= steps
 
   dirs.split(",").each do |dir|
     case dir[0]
@@ -37,9 +53,10 @@ def plot(dirs)
 
     count = Integer(dir[1..-1])
     count.times do
+      steps += 1
       pos[0] += delta[0]
       pos[1] += delta[1]
-      wire << pos.dup
+      wire[pos.dup] ||= steps
     end
   end
 
@@ -47,5 +64,9 @@ def plot(dirs)
 end
 
 if $0 == __FILE__
-  puts crossing_distance(File.read("input.txt"))
+  input = File.read("input.txt")
+  puts "Part 1"
+  puts crossing_distance(input)
+  puts "Part 2"
+  puts crossing_steps(input)
 end
