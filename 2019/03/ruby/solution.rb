8cab3ec2d31516e7ebf2f560fb285b8c2aede2da
Martin Vidner <martin@vidner.net>
2019-12-05 00:24:53 +0100

    Render the wires as PNG (but it rus out of memory)

--- 2019/03/ruby/solution.rb
+++ 2019/03/ruby/solution.rb
@@ -63,10 +63,49 @@ def plot(dirs)
   wire
 end
 
+def make_png(input, png_filename)
+  lines = input.split "\n"
+  wire1 = plot(lines[0])
+  wire2 = plot(lines[1])
+
+  all_points = wire1.merge(wire2).keys
+  minx, maxx = all_points.map(&:first).minmax
+  miny, maxy = all_points.map(&:last).minmax
+
+  pngx = maxx - minx + 1
+  pngy = maxy - miny + 1
+  p [pngx, pngy]
+
+  png = ChunkyPNG::Image.new(pngx, pngy, ChunkyPNG::Color::WHITE)
+  red = ChunkyPNG::Color("red")
+  green = ChunkyPNG::Color("green")
+  blue = ChunkyPNG::Color("blue")
+
+  wire1.keys.each do |x, y|
+    png[x - minx, y - miny] = red
+  end
+  wire2.keys.each do |x, y|
+    png[x - minx, y - miny] = blue
+  end
+  crossings = Set.new(wire1.keys).intersection(Set.new(wire2.keys))
+  crossings.each do |x, y|
+    png[x - minx, y - miny] = green
+  end
+
+  png.save(png_filename)
+end
+
 if $0 == __FILE__
   input = File.read("input.txt")
   puts "Part 1"
   puts crossing_distance(input)
   puts "Part 2"
   puts crossing_steps(input)
+
+  begin
+    require "chunky_png"
+    puts "Image"
+    make_png(input, "wires.png")
+  rescue LoadError
+  end
 end
