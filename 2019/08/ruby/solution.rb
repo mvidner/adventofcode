a90ee0bb90b01328b4aac8b8c2c6ad53c2d568c1
Martin Vidner <martin@vidner.net>
2019-12-08 18:40:33 +0100

    Solved 2019/08 p 2

--- 2019/08/ruby/solution.rb
+++ 2019/08/ruby/solution.rb
@@ -15,11 +15,27 @@ class SpaceImageFormat
   def layer(i)
     data[i * w * h, w * h]
   end
+
+  def combine(top_layer, bottom_layer)
+    # puts "Combining", displayable(top_layer), "and", displayable(bottom_layer)
+    r = top_layer.chars.zip(bottom_layer.chars).map do |tc, bc|
+      tc == "2" ? bc : tc
+    end.join ""
+    # puts "Result", displayable(r)
+    r
+  end
+
+  def displayable(layer)
+    layer.chars.each_slice(w).map do |r|
+      r.join ""
+    end.join "\n"
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
   img = SpaceImageFormat.new(25, 6, File.read("input.txt").chomp)
   puts "Part 1"
+  puts "#{img.nlayers} layers"
   min0layer = (0...img.nlayers)
               .map { |i| [i, img.layer(i).count("0")] }
               .min_by { |_i, count| count }
@@ -28,5 +44,10 @@ if $PROGRAM_NAME == __FILE__
   puts layer.count("1") * layer.count("2")
 
   puts "Part 2"
-  puts "?"
+  composite_layer = "2" * img.w * img.h
+  (0...img.nlayers).each_with_object(composite_layer) do |i, clayer|
+    # puts "WITH LAYER #{i}"
+    composite_layer.replace img.combine(clayer, img.layer(i))
+  end
+  puts img.displayable(composite_layer).tr("01", " X")
 end
