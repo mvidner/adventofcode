976bd4f5c4fac3fada7286a7ae85b918e8ce13c0
Martin Vidner <martin@vidner.net>
2019-12-08 17:36:08 +0100

    Solved 2019/07 part 2

--- 2019/07/ruby/solution.rb
+++ 2019/07/ruby/solution.rb
@@ -11,11 +11,14 @@ class Intcode
   attr_reader :inq
   # @return [Queue] output queue
   attr_reader :outq
+  # @return [String] for debugging
+  attr_reader :name
 
-  def initialize(mem, inq, outq)
+  def initialize(mem, inq, outq, name: "Intcode")
     @m = mem
     @inq = inq
     @outq = outq
+    @name = name
     @ip = 0
   end
 
@@ -41,11 +44,17 @@ class Intcode
       # INPUT
       when 3
         nargs = 1
-        write(0, inq.pop)
+        # puts "#{name}: INqsize #{inq.size}"
+        val = inq.pop
+        write(0, val)
+        # puts "#{name}: IN #{val}"
       # OUTPUT
       when 4
         nargs = 1
-        outq.push(read(0))
+        val = read(0)
+        # puts "#{name}: OUTqsize #{outq.size}"
+        outq.push(val)
+        # puts "#{name}: OUT #{val}"
       # JUMP-IF-TRUE
       when 5
         nargs = 2
@@ -121,17 +130,52 @@ class AmplifierCircuit
   end
 
   def run(input)
-    @phases.each do |ph|
+    @phases.each_with_index do |ph, i|
       inq = Queue.new
       inq.push(ph)
       inq.push(input)
       outq = Queue.new
-      ic = Intcode.new(@progmem.dup, inq, outq)
+      ic = Intcode.new(@progmem.dup, inq, outq, name: "AMP#{i}(p#{ph})")
       ic.compute
       input = outq.pop
     end
 
     input
+  end
+
+  def run_parallel(input)
+    n = @phases.size
+    queues = @phases.map do |ph|
+      q = Queue.new
+      q.push(ph)
+      # puts "#{q} size #{q.size}"
+      q
+    end
+
+    amps = (0...n).map do |i|
+      inq = queues[i % n]
+      outq = queues[(i + 1) % n]
+      # puts "#{inq.inspect} > Amp#{i} > #{outq.inspect}"
+      Intcode.new(@progmem.dup, inq, outq, name: "Amp#{i}")
+    end
+
+    threads = amps.map do |amp|
+      t = Thread.new do
+        # sleep(rand)
+        amp.compute
+      end
+      t.name = amp.name
+      t
+    end
+
+    queues.first.push(input)
+    threads.each(&:join)
+    # queues.each { |q| puts "#{q} #{q.size} #{q.num_waiting}" }
+
+    # Only after they all ran, otherwise we steal the last amp's phase setting
+    # The LAST Amp outputs to the FIRST queue!
+    result = queues.first.pop
+    result
   end
 
   def self.find_max_amp(progmem)
@@ -149,12 +193,30 @@ class AmplifierCircuit
     puts "Max amp #{max_amp} for phase #{max_phase}"
     max_amp
   end
+
+  def self.find_max_feedback_amp(progmem)
+    max_amp = -1
+    max_phase = nil
+    (5..9).to_a.permutation do |phase|
+      # puts "Trying phases #{phase.inspect}"
+      ic = new(progmem, phase)
+      amp = ic.run_parallel(0)
+
+      if amp > max_amp
+        max_phase = phase
+        max_amp = amp
+      end
+    end
+    puts "Max feedback amp #{max_amp} for phase #{max_phase}"
+    max_amp
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
+  progmem = Intcode.read_file("input.txt")
   puts "Part 1"
-  puts AmplifierCircuit.find_max_amp(Intcode.read_file("input.txt"))
+  puts AmplifierCircuit.find_max_amp(progmem)
 
   puts "Part 2"
-  puts "?"
+  puts AmplifierCircuit.find_max_feedback_amp(progmem)
 end
