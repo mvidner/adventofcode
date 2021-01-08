22cdafc9e50c6d63bcda4117c042f45940fddbcb
Martin Vidner <martin@vidner.net>
2021-01-08 19:02:25 +0100

    Solved 2020/01 p 2

--- 2020/01/ruby/solution.rb
+++ 2020/01/ruby/solution.rb
@@ -7,17 +7,33 @@ class ReportRepair
   end
 
   def find_2020
+    a, b = find_pair(2020)
+    a * b
+  end
+
+  def find_pair(sum)
     # since the entries are bounded we can use them as members in a set
     seen = Set.new
 
     @entries.each do |e|
-      other = 2020 - e
-      if seen.include?(other)
-        return e * other
-      end
+      other = sum - e
+      return [e, other] if seen.include?(other)
+
       seen << e
     end
     raise "No matching pair found"
+  end
+
+  def find_triplet(sum)
+    # brute force for a change
+    @entries.each do |a|
+      @entries.each do |b|
+        @entries.each do |c|
+          return [a, b, c] if a + b + c == sum
+        end
+      end
+    end
+    raise "No matching triplet found"
   end
 end
 
@@ -25,4 +41,8 @@ if $PROGRAM_NAME == __FILE__
   rr = ReportRepair.new(File.read("input.txt"))
   puts "Part 1"
   puts rr.find_2020
+
+  puts "Part 2"
+  a, b, c = rr.find_triplet(2020)
+  puts a * b * c
 end
--- 2020/01/ruby/spec/solution_spec.rb
+++ 2020/01/ruby/spec/solution_spec.rb
@@ -3,22 +3,26 @@ require_relative "../solution"
 describe ReportRepair do
   subject { described_class.new(sample) }
 
-  describe "#find_2020" do
-    let(:sample) do
-      <<~TXT
-      1721
-      979
-      366
-      299
-      675
-      1456
-      TXT
-    end
+  let(:sample) do
+    <<~TXT
+    1721
+    979
+    366
+    299
+    675
+    1456
+    TXT
+  end
 
+  describe "#find_2020" do
     it "works for the sample" do
-      expect(subject.find_2020).to eq 514579
+      expect(subject.find_2020).to eq 514_579
+    end
+  end
+
+  describe "#find_triplet" do
+    it "works for the sample" do
+      expect(subject.find_triplet(2020)).to eq [979, 366, 675]
     end
   end
 end
-
-
