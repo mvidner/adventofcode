8b15d2033703efe816b1d246386d3618f66102b3
Martin Vidner <martin@vidner.net>
2021-01-09 13:01:05 +0100

    Stated and solved 2020/02 p2

--- 2020/02/ruby/solution.rb
+++ 2020/02/ruby/solution.rb
@@ -8,6 +8,10 @@ class PasswordPhilosophy
     @input.lines.count { |l| valid?(l) }
   end
 
+  def count_newly_valid_passwords
+    @input.lines.count { |l| newly_valid?(l) }
+  end
+
   def valid?(line)
     line =~ /(\d+)-(\d+) (.): (.*)/
     min = $1.to_i
@@ -17,6 +21,19 @@ class PasswordPhilosophy
 
     char_count = tally(pass.chars).fetch(char, 0)
     (min..max).include?(char_count)
+  end
+
+  def newly_valid?(line)
+    line =~ /(\d+)-(\d+) (.): (.*)/
+    first = $1.to_i
+    second = $2.to_i
+    char = $3
+    pass = $4
+
+    first_match = pass[first - 1] == char
+    second_match = pass[second - 1] == char
+
+    first_match != second_match
   end
 
   def tally(enumerable)
@@ -34,6 +51,6 @@ if $PROGRAM_NAME == __FILE__
   puts "Part 1"
   puts sol.count_valid_passwords
 
-  #puts "Part 2"
-  #puts sol
+  puts "Part 2"
+  puts sol.count_newly_valid_passwords
 end
