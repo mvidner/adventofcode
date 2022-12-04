be7bde0530f23c72ba45ff8fb3eb0a2b547beb01
Martin Vidner <martin@vidner.net>
2022-12-04 14:33:29 +0100

    2022/04 Camp Cleanup solved p2

--- 2022/04/ruby/solution.rb
+++ 2022/04/ruby/solution.rb
@@ -9,12 +9,26 @@ def range_cover?(outer, inner)
   outer.cover?(inner.begin) && outer.cover?(inner.end)
 end
 
-contained = data.find_all do |line|
+def range_overlap?(a, b)
+  a.cover?(b.begin) || a.cover?(b.end) ||
+    b.cover?(a.begin) || b.cover?(a.end)
+end
+
+ranges = data.map do |line|
   as, bs = line.split /,/
   ar = Range.new(* as.split(/-/).map(&:to_i))
   br = Range.new(* bs.split(/-/).map(&:to_i))
+  [ar, br]
+end
 
+contained = ranges.find_all do |ar, br|
   range_cover?(ar, br) || range_cover?(br, ar)
 end
 
 puts "Pairs with containment: #{contained.size}"
+
+overlapping = ranges.find_all do |ar, br|
+  range_overlap?(ar, br)
+end
+
+puts "Pairs with overlap: #{overlapping.size}"
