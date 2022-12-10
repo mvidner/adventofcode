afa16efced0756e4bd983b82ea02fbb2fd6339b3
Martin Vidner <martin@vidner.net>
2022-12-10 10:45:02 +0100

    2022/10 p2 solved

--- 2022/10/ruby/solution.rb
+++ 2022/10/ruby/solution.rb
@@ -1,23 +1,42 @@
 #!/usr/bin/ruby
 
 class CRT
+  def initialize
+    @pixels = " " * 240
+  end
+
+  def dump
+    6.times do |row|
+      puts @pixels[row*40, 40]
+    end
+  end
+
+  # clock is 1 based but pixels are 0 based
+  def draw(clock, sprite_pos)
+    column = (clock - 1) % 40
+    if (sprite_pos - 1 .. sprite_pos + 1).cover?(column)
+      pix = "#"
+    else
+      pix = "."
+    end
+    @pixels[clock - 1] = pix
+  end
+end
+
+class CPU
   attr_accessor :x
 
   # already elapsed
   attr_accessor :cycles
 
-  attr_reader :strengths_sum
-
   def initialize
     @x = 1
     @cycles = 0
-    @important_cycles = [20, 60, 100, 140, 180, 220]
-    @strengths_sum  = 0
   end
 
   def do1(insn)
     @cycles += 1
-    cycle_check
+    yield cycles, x
 
     case insn
     when "noop"
@@ -25,7 +44,7 @@ class CRT
     when /addx (-?\d+)/
       # second cycle: check _during_ it
       @cycles += 1
-      cycle_check
+      yield cycles, x
 
       # ... add only at its end
       @x += $1.to_i
@@ -49,10 +68,13 @@ if $PROGRAM_NAME == __FILE__
   input = text.lines.map(&:chomp)
 
   crt = CRT.new
+  cpu = CPU.new
   input.each do |insn|
-    puts "  (#{crt.cycles}: #{crt.x}) #{insn}"
-    crt.do1(insn)
+    # puts "  (#{crt.cycles}: #{crt.x}) #{insn}"
+    cpu.do1(insn) do |cycles, x|
+      crt.draw(cycles, x)
+    end
   end
 
-  puts crt.strengths_sum
+  crt.dump
 end
