e19aec128a9c7733fb342fc7c664fe52b0a31afa
Martin Vidner <martin@vidner.net>
2022-12-14 20:25:56 +0100

    Solved 2022/14 p1

--- 2022/14/ruby/solution.rb
+++ 2022/14/ruby/solution.rb
@@ -49,11 +49,10 @@ class Sandfall
 
     @source = Point.new(500, 0)
     set(@source, "+")
-
-    dump
   end
 
   def dump
+    puts
     puts @rows
   end
 
@@ -61,11 +60,18 @@ class Sandfall
     get2(p.x, p.y)
   end
 
+  # @return nil if outside the map
   def get2(x, y)
-    @rows[y - @miny][x - @minx]
+    yy = y - @miny
+    return nil if yy < 0
+
+    xx = x - @minx
+    return nil if xx < 0
+
+    @rows.fetch(yy, [])[xx]
   end
 
-  def set(p, val)      
+  def set(p, val)
     set2(p.x, p.y, val)
   end
 
@@ -73,7 +79,49 @@ class Sandfall
     # p "set2 #{x} #{y} #{val}"
     @rows[y - @miny][x - @minx] = val
   end
-  
+
+  # Drop one unit of sand from the source and mark it on the map
+  # Return false if it would go outside the map
+  def drop
+    prev = nil
+    sand = @source.dup
+
+    while sand != prev
+      sand, prev = drop_step(sand), sand
+      return false if sand.nil?
+    end
+
+    set(sand, "o")
+    true
+  end
+
+  def drop_step(p)
+    tries = [
+      Point.new(p.x,     p.y + 1),
+      Point.new(p.x - 1, p.y + 1),
+      Point.new(p.x + 1, p.y + 1),
+      Point.new(p.x,     p.y    ),
+    ]
+    found = tries.find do |t|
+      got = get(t)
+      return nil if got.nil?
+      got== "."
+    end
+    raise "should not happen" if found.nil?
+    found
+  end
+
+  def pour
+    dropped = 0
+    while drop
+      dropped += 1
+      dump
+    end
+    puts "Dropped #{dropped}"
+  end
+
+  private
+
   def draw_segment(from, to)
     if from.x == to.x
       draw_v(from, to)
@@ -98,4 +146,5 @@ end
 if $PROGRAM_NAME == __FILE__
   text = File.read(ARGV[0] || "input.txt")
   sf = Sandfall.parse(text)
+  sf.pour
 end
