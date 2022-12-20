e38209939ea6c8a75fe38fe6620e4f737fc7a8cf
Martin Vidner <martin@vidner.net>
2022-12-20 15:28:19 +0100

    2022/20 not working but realized the wraparound bug
    The puzzle specifies a 1 jump boost at wraparound, WTF

--- 2022/20/ruby/solution.rb
+++ 2022/20/ruby/solution.rb
@@ -18,7 +18,8 @@ class Mixer
 
     @size.times do |i|
       np = @new_pos[i]
-      move(np, @numbers[i])
+      puts "Time #{i}, what was at #{i} is now at #{np}"
+      move(np, @numbers[np])
     end
   end
 
@@ -40,58 +41,30 @@ class Mixer
     if delta == 0
       puts "#{@numbers[from]} does not move:" if tracing?
       dump if tracing?
-
       return
     end
 
-    print "at #{from} by #{delta} "
-    to = (from + delta) % @size
+    to = from + delta
+    puts "UNADJ #{from} + #{delta} -> #{to}"
+    # print "at #{from} by #{delta} "
+    to = to % @size
+    puts "A TO  #{from} + #{delta} -> #{to}"
+    delta2 = delta % @size
+    to2 = from + delta
+    puts "A DE  #{from} + #{delta2} -> #{to2} (#{to2 % @size})"
+
+
     raise if to < 0
     delta = to - from
     puts "adjusted to #{delta}"
-    # remember to adjust new_pos of all the in-betweens!
-
-    # front[] thing back[]
-
-    # delta < 0
-    # front1[] thing front2[] back[]
-
-    # delta > 0
-    # front[] back1[] thing back2[]
-
-    front = @numbers[0 ... from]
-    thing = @numbers[from]
-    back = @numbers[from + 1 .. @size]
 
     dist = delta.abs
-
-    if delta < 0
-      front1, front2 = front[0 ... from - dist], front[from .. -1]
-      back1, back2 = [], back
-
-      left = front1.last
-      right = front2.first
-    else # > 0
-      front1, front2 = front, []
-      back1, back2 = back[0 ... dist], back[dist .. -1]
-
-      left = back1.last
-      right = back2.first
+    delta1 = delta / dist # -1 or +1
+    puts "D1 #{delta1}"
+    dist.times do
+      swap(from, from + delta1)
+      from += delta1
     end
-    puts "Before #{front1.inspect} #{front2.inspect} #{thing} #{back1.inspect} #{back2.inspect}" if tracing?
-    puts "#{thing} moves between #{left} and #{right}" if tracing?
-
-    if delta < 0
-      save_numbers = @numbers[to]
-      @numbers[to] = @numbers[from]
-
-      save_old = @old_pos[to]
-      @old_pos[to] = @old_pos[from]
-      save_new
-      @new_pos[]
-    else
-    end
-    @new_pos[from] = to
 
     puts "OLD: #{@old_pos.inspect}"
     puts "NEW: #{@new_pos.inspect}"
@@ -111,6 +84,7 @@ class Mixer
   end
 
   def swap(i, j)
+    puts "SWAP #{i} #{j}"
     array_swap(@numbers, i, j)
 
     npi, npj = @new_pos[i], @new_pos[j]
@@ -120,8 +94,10 @@ class Mixer
     @new_pos[opi] = j
     @old_pos[npj] = i
     @old_pos[npi] = j
+    print "    "
+    dump
   end
-  
+
 
   def find_past(mark)
     -1
