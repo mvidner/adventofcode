e521b0eabe8ef9e679ac301e93938b280da882b0
Martin Vidner <martin@vidner.net>
2022-12-22 22:20:11 +0100

    2022/17 Pyroclastic Flow, weightless tiles

--- 2022/17/ruby/solution.rb
+++ 2022/17/ruby/solution.rb
@@ -9,7 +9,7 @@ end
 
 tiles = [
   [
-    "####"    
+    "####"
   ],
   [
     ".#.",
@@ -17,9 +17,10 @@ tiles = [
     ".#."
   ],
   [
+    # bottom line has index 0
+    "###",
     "..#",
-    "..#",
-    "###"
+    "..#"
   ],
   [
     "#",
@@ -33,14 +34,68 @@ tiles = [
   ],
 ]
 
-TILES = tiles.map { |rows| Tile.new(rows) }
-
 class Tile
   def initialize(rows)
+    @rows = rows
   end
+
+  attr_reader :rows
 end
 
+TILES = tiles.map { |rows| Tile.new(rows) }
+
 class Tetris
+  def initialize(moves)
+    @moves = moves
+    @next_move = 0
+
+    @width = 7
+    @height = 0
+    @next_tile = 0
+
+    # row 0 is bottom
+    @rows = []
+  end
+
+  # distance of topmost rock from the floor
+  attr_reader :height
+
+  def dump
+    (@height - 1).downto(0) do |ri|
+      puts "|#{@rows[ri]}| #{ri}"
+    end
+    puts "+#{"-" * @width}+"
+  end
+
+  # Drop one tile until it lands
+  # using *moves* on it as it falls
+  def drop
+    tile = TILES[@next_tile]
+    @next_tile = (@next_tile + 1) % TILES.size
+
+    spawn(tile)
+
+  end
+
+  def make_rows(count)
+    Array.new(count) { "." * @width }
+  end
+
+  LEFT_PAD = 2
+  BOTTOM_PAD = 3
+
+  def spawn(tile)
+    @rows.concat(make_rows(BOTTOM_PAD))
+    @height += BOTTOM_PAD
+
+    tile_height = tile.rows.size
+    fresh_rows = make_rows(tile_height)
+    fresh_rows.zip(tile.rows).each do |fr, tr|
+      fr[2, tr.size] = tr
+    end
+    @rows.concat(fresh_rows)
+    @height += tile_height
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
@@ -48,4 +103,14 @@ if $PROGRAM_NAME == __FILE__
   moves = text.chomp.chars
 
   tetris = Tetris.new(moves)
+  puts "Initially"
+  tetris.dump
+  2022.times do |i|
+    tetris.drop
+    tetris.dump
+  end
+  puts "Finally"
+  tetris.dump
+  puts "Height: #{tetris.height}"
+
 end
