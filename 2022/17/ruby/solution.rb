63b8a66d43ee2dcbfe8c86123a9401b0b2ac9fb9
Martin Vidner <martin@vidner.net>
2022-12-23 22:09:39 +0100

    2022/17 coding with bit masks

--- 2022/17/ruby/solution.rb
+++ 2022/17/ruby/solution.rb
@@ -7,7 +7,12 @@ Pos = Struct.new(:r, :c) do
   end
 end
 
-tiles = [
+# Noticing that the collision detection works on black and white,
+# and that we may need very efficient storage,
+# I' represent the board positions, and tile units, with BITS in integers
+# (and maybe in ASCII characters)
+
+tile_shapes = [
   [
     "####"
   ],
@@ -17,10 +22,9 @@ tiles = [
     ".#."
   ],
   [
-    # bottom line has index 0
-    "###",
     "..#",
-    "..#"
+    "..#",
+    "###"
   ],
   [
     "#",
@@ -34,76 +38,148 @@ tiles = [
   ],
 ]
 
-class Tile
-  def initialize(rows)
-    @rows = rows
+# A tile is a rectangle of empty spaces and rocks, represented by
+# an array (lines) of bits (0 space, 1 rock) representing Integers
+# (later maybe compress it further into a binary string)
+# where the LSB is at the right,
+# and first row is the BOTTOM one.
+class Tile < Array
+  def self.parse_shape(shape_lines)
+    # input is visual, first at top, reverse it for first at bottom
+    rows = shape_lines.reverse.map do |l|
+      l.tr(".#", "01").to_i(2)
+    end
+    new(rows)
   end
 
-  attr_reader :rows
+  def initialize(rows, width: nil)
+    super()
+    replace(rows)
+    @width = width || self.map { |r| r.bit_length }.max
+  end
+
+  attr_reader :width
+
+  # @param count [Integer] may be negative to shift left
+  # @return [Tile] a new tile, or nil if we'd shift outside valid_width
+  def shift_right(count, valid_width)
+    raise ArgumentError if count == 0
+
+    # should unit test this :-)
+    if count > 0
+      fail_mask = (1 << count) - 1
+    else
+      fail_mask = ~((1 << (valid_width + count)) - 1)
+    end
+
+    nrows = self.map do |r|
+      return nil if r & fail_mask != 0
+      r >> count
+    end
+    Tile.new(nrows, width: @width)
+  end
 end
 
-TILES = tiles.map { |rows| Tile.new(rows) }
+TILES = tile_shapes.map { |rows| Tile.parse_shape(rows) }
 
 class Tetris
+  LEFT_PAD = 2
+  BOTTOM_PAD = 3
+  WIDTH = 7
+  # DUMP_PADDING = "." * WIDTH
+
   def initialize(moves)
-    @moves = moves
     puts "#{moves.size} moves"
+    @moves = moves
     @next_move = 0
 
-    @width = 7
     @height = 0
-    @next_tile = 0
+    @next_tile_i = 0
 
     # row 0 is bottom
+    # row elements are bits as integers, see Tile
     @rows = []
+    @tile = []
   end
 
   # distance of topmost rock from the floor
   attr_reader :height
 
-  def dump
-    (@height - 1).downto(0) do |ri|
-      puts "|#{@rows[ri]}| #{ri}"
+  def dump_part(rows, visual, row_offset: 0)
+    dump_padding = visual[0] * WIDTH
+    (rows.size - 1).downto(0) do |ri|
+      r = rows[ri]
+      sr = dump_padding + r.to_s(2).tr("01", visual)
+      sr = sr[-WIDTH .. -1]
+      puts "|#{sr}| #{ri + row_offset}"
     end
-    puts "+#{"-" * @width}+"
+  end
+
+  def dump
+    puts "TILE"
+    dump_part(@tile, " @", row_offset: @tile_bottom)
+    puts
+
+    puts "TOWER"
+    dump_part(@rows, ".#")
+    puts "+#{"-" * WIDTH}+"
+    puts
   end
 
   # Drop one tile until it lands
   # using *moves* on it as it falls
   def drop
-    tile = TILES[@next_tile]
-    @next_tile = (@next_tile + 1) % TILES.size
-
-    spawn(tile)
+    spawn
 
     begin
       push
       landed = drop1
     end until landed
 
-  end
-
-  def drop1
+    commit
   end
 
   def make_rows(count)
-    Array.new(count) { "." * @width }
+    Array.new(count, 0)
   end
 
-  LEFT_PAD = 2
-  BOTTOM_PAD = 3
+  def spawn
+    @tile = TILES[@next_tile_i]
+    @next_tile_i = (@next_tile_i + 1) % TILES.size
 
-  def spawn(tile)
-    @rows.concat(make_rows(BOTTOM_PAD))
-    @height += BOTTOM_PAD
+#    @rows.concat(make_rows(BOTTOM_PAD))
+#    @height += BOTTOM_PAD
 
-    tile_height = tile.rows.size
-    fresh_rows = make_rows(tile_height)
-    fresh_rows.zip(tile.rows).each do |fr, tr|
-      fr[2, tr.size] = tr
+    # row where the bottom-most piece of the tile is
+    @tile_bottom = @height + BOTTOM_PAD
+
+    # unnecessary rendering?
+#    tile_height = @tile.size
+#    fresh_rows = @tile.shift_right(-(WIDTH-@tile.width-LEFT_PAD))
+#    @rows.concat(fresh_rows)
+#    @height += tile_height
+
+    @tile = @tile.shift_right(-(WIDTH-@tile.width-LEFT_PAD), WIDTH)
+  end
+
+  def push
+    move = @moves[@next_move]
+    @next_move = (@next_move + 1) % @moves.size
+
+    # DEBUG
+    5.times do
+      t = @tile.shift_right(move == ">" ? 1 : -1, WIDTH)
+      @tile = t if t
     end
-    @rows.concat(fresh_rows)
-    @height += tile_height
+#    (@tile_bottom ... @height).each do |ri|
+  end
+
+  # @return [Boolean] true if landed
+  def drop1
+    true
+  end
+
+  def commit
   end
 end
 
@@ -114,7 +190,8 @@ if $PROGRAM_NAME == __FILE__
   tetris = Tetris.new(moves)
   puts "Initially"
   tetris.dump
-  2022.times do |i|
+  #2022.times do |i|
+  22.times do |i|
     tetris.drop
     tetris.dump
   end
