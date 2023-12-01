43de3eeb77b697de6ed8bb83fd9499d28c1be7ae
Martin Vidner <martin@vidner.net>
2023-12-01 21:19:40 +0100

    2023/01 p2 wrong solution
    53846 is too low

--- 2023/01/js/solution.js
+++ 2023/01/js/solution.js
@@ -27,6 +27,52 @@ function solve(lines) {
     console.log('sum:', sum);
 }
 
+const digits = {
+    "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+}
+
+const digitWords = Object.keys(digits);
+
+function digitToInt(s) {
+    if (s.length === 1) {
+        return Number.parseInt(s);
+    }
+    else {
+        return digits[s];
+    }
+}
+
+function digits2(line) {
+    const ds = line.match(/[1-9]|one|two|three|four|five|six|seven|eight|nine/g);
+    return ds;
+}
+
+function firstDigit2(line) {
+    const ds = digits2(line);
+    if (ds === null) {
+        return null;
+    }
+    return digitToInt(ds[0]);
+}
+
+function lastDigit2(line) {
+    const ds = digits2(line);
+    if (ds === null) {
+        return null;
+    }
+    return digitToInt(ds[ds.length - 1]);
+}
+
+function solve2(lines) {
+    let sum = 0;
+    lines.forEach(line => {
+        const d1 = firstDigit2(line);
+        const d2 = lastDigit2(line);
+        sum = sum + 10 * d1 + d2;
+    });
+    console.log('improved sum:', sum);
+}
+
 let fname = 'input.txt';
 if (process.argv[2]) {
     fname = process.argv[2]
@@ -36,6 +82,7 @@ try {
     const data = fs.readFileSync(fname, 'utf8');
     const lines = data.split('\n');
     solve(lines);
+    solve2(lines);
 } catch (err) {
     console.error(err);
 }
