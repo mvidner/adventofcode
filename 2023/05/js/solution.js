c1ce910e2d331fe15b25f327ca7bcf810a69fc2e
Martin Vidner <martin@vidner.net>
2023-12-06 20:23:43 +0100

    2023/05 p1 solved

--- 2023/05/js/solution.js
+++ 2023/05/js/solution.js
@@ -22,12 +22,32 @@ class AlmanacRange {
         this.src = src;
         this.len = len;
     }
+
+    // if _n_ is in the range, map it to its destination, otherwise return null
+    // (hmm what's an idiomatic way to return Option<Integer>?)
+    map(n) {
+        if ((this.src <= n) && (n < this.src + this.len)) {
+            return n - this.src + this.dest;
+        }
+        return null;
+    }
 }
 
 class AlmanacMap {
     constructor() {
         this.name = "?";
         this.ranges = [];
+    }
+
+    map(n) {
+        // stupid way should work
+        const mapper = this.ranges.find(r => (r.map(n) !== null));
+        if (mapper === undefined) {
+            return n;
+        }
+        else {
+            return mapper.map(n);
+        }
     }
 }
 
@@ -67,7 +87,23 @@ class Almanac {
         return rest[1].split(/ +/).map(s => (Number.parseInt(s)));
     }
 
+    // maps a seed number to a location number
+    map(seedN) {
+        let n = seedN;
+        // reduce should also work
+        this.maps.forEach(m => {
+            n = m.map(n);
+        });
+        return n;
+    }
+
     solve() {
+        // why can't I use just Math.min in reduce?
+        const pairMin = (a, b) => Math.min(a, b);
+        const locations = this.seeds.map(n => this.map(n));
+
+        const minLoc = locations.reduce(pairMin);
+        console.log("mininal location", minLoc);
     }
 }
 
