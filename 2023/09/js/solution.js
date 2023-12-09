3afa758f10a5e2fe8061bc228b209728881a6b65
Martin Vidner <martin@vidner.net>
2023-12-09 22:14:33 +0100

    2023/09 Mirage Maintenance (seq extrapolation) p1 solved

---
+++ 2023/09/js/solution.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+"use strict";
+const fs = require('node:fs');
+
+// Read input.txt or another file, the script argument
+// return array of string Not including \n
+function inputLines() {
+    let fname = 'input.txt';
+    if (process.argv[2]) {
+        fname = process.argv[2]
+    }
+
+    const data = fs.readFileSync(fname, 'utf8');
+    let lines = data.split('\n');
+    lines.pop();
+    return lines;
+}
+
+
+// "12 13    42" -> [12, 13, 42]
+function parseNums(s) {
+    return s.split(/\s+/).map(sn => (Number.parseInt(sn)))
+}
+
+class Sequence {
+    constructor(ns) {
+        this.ns = ns;
+    }
+
+    extrapolate() {
+        if (this.ns.every(n => (n === 0))) {
+            return 0;
+        }
+        else {
+            const diffs = this.differences();
+            const delta = diffs.extrapolate();
+            return this.ns[this.ns.length - 1] + delta;
+        }
+    }
+
+    differences() {
+        let ds = [];
+        for (let i = 1; i < this.ns.length; ++i) {
+            ds.push(this.ns[i] - this.ns[i - 1]);
+        }
+        return new Sequence(ds);
+    }
+}
+
+function sum(ns) {
+    return ns.reduce((e, a) => (e + a));
+}
+
+function solve(sequences) {
+    const nexts = sequences.map(seq => (seq.extrapolate()));
+    const s = sum(nexts);
+    console.log("sum of extrapolated values", s);
+}
+
+const lines = inputLines();
+const sequences = lines.map(l => (new Sequence(parseNums(l))));
+solve(sequences);
