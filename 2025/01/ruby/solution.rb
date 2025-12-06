25b1bbb90588d97d34e74a20ff7b46178075ff2f
Martin Vidner <martin@vidner.net>
2025-12-06 13:07:01 +0100

    2025/01 p2 solved

--- 2025/01/ruby/solution.rb
+++ 2025/01/ruby/solution.rb
@@ -16,10 +16,15 @@ end.compact
 
 position = 50
 at_zero_count = 0
+at_zero_clicks = 0
 
 data.each do |d|
+  zero_clicks = ((position + d) / 100.0).floor.abs
+  at_zero_clicks += zero_clicks
+
   position = (position + d) % 100
   at_zero_count +=1 if position.zero?
 end
 
 puts "#{at_zero_count} times zero position"
+puts "#{at_zero_clicks} times clicked at zero"
