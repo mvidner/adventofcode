89b7f80a6a2f880383c6bceaf22e56c1cae2f638
Martin Vidner <martin@vidner.net>
2025-12-07 19:24:40 +0100

    2025/03 p2 solved

--- 2025/03/ruby/solution.rb
+++ 2025/03/ruby/solution.rb
@@ -1,12 +1,28 @@
 #!/usr/bin/env ruby
 text = File.read(ARGV[0] || "input.txt")
 
-joltages = text.lines.map do |l|
-  nums = l.chomp.chars.map(&:to_i)
-  # max_by returns all maximums, we take the first one
-  first, where = nums[0..-2].each_with_index.to_a.max_by { |n, _i| n }
-  second = nums[where + 1..-1].max
-  10 * first + second
+def joltage_nums(nums, size)
+  return [nums.max] if size == 1
+
+  first, where = nums[0..-size].each_with_index.to_a.max_by { |n, _i| n }
+  rest = joltage_nums(nums[where + 1..-1], size - 1)
+  [first, *rest]
 end
 
+def joltage(nums, size)
+  joltage_nums(nums, size).map(&:to_s).join.to_i
+end
+
+all_nums = text.lines.map do |l|
+  l.chomp.chars.map(&:to_i)
+end
+
+joltages = all_nums.map do |nums|
+  joltage(nums, 2)
+end
 puts "Joltages sum #{joltages.sum}"
+
+large_joltages = all_nums.map do |nums|
+  joltage(nums, 12)
+end
+puts "Large joltages sum #{large_joltages.sum}"
