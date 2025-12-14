8e0c9410ef563f654f71a143c9a42c3b5b5364d7
Martin Vidner <martin@vidner.net>
2025-12-14 11:39:26 +0100

    2024/04 p2 solved
    run time 11s, 85 iterations

--- 2025/04/ruby/solution.rb
+++ 2025/04/ruby/solution.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 
 class Grid
-  def initialize(filename)
+  def self.from_file(filename)
     text = File.read(filename)
-    @lines = text.lines.map(&:chomp)
+    lines = text.lines.map(&:chomp)
+    new(lines)
+  end
+
+  def initialize(lines)
+    @lines = lines
   end
 
   # @return [Character] " " if out of bounds
@@ -50,7 +55,48 @@ class Grid
     end
     sum
   end
+
+  # @return [Grid] a new Grid with the accessible rolls removed
+  def remove_accessible_rolls
+    new_lines = Array.new(@lines.size)
+
+    @lines.each_with_index do |line, row|
+      new_line = " " * line.size
+
+      line.chars.each_with_index do |char, col|
+        if char == "@"
+          count = count_neighboring_rolls(row, col)
+          if count < 4
+            new_line[col] = "x"
+          else
+            new_line[col] = "@"
+          end
+        else
+          new_line[col] = "."
+        end
+      end
+      new_lines[row] = new_line
+    end
+    Grid.new(new_lines)
+  end
+
+  def print
+    puts @lines
+  end
 end
 
-grid = Grid.new(ARGV[0] || "input.txt")
+grid = Grid.from_file(ARGV[0] || "input.txt")
+grid.print
 puts "#{grid.count_accessible_rolls} accessible rolls"
+
+total_accessible_rolls = 0
+i = 0
+loop do
+  accessible_rolls = grid.count_accessible_rolls
+  break if accessible_rolls == 0
+
+  total_accessible_rolls += accessible_rolls
+  i += 1
+  grid = grid.remove_accessible_rolls
+end
+puts "#{total_accessible_rolls} total accessible rolls in #{i} iterations"
