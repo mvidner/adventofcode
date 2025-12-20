f9544e56134c87ab00ad18d90f401f810bd45a1b
Martin Vidner <martin@vidner.net>
2025-12-20 13:08:16 +0100

    2025/09 p2: wrong solution
    returns 21 for sample instead of 24

--- 2025/09/ruby/solution.rb
+++ 2025/09/ruby/solution.rb
@@ -11,9 +11,24 @@ Point = Struct.new(:x, :y) do
   def rectangle_area(other)
     ((x - other.x).abs + 1) * ((y - other.y).abs + 1)
   end
+
+  # @return [(Point, Point, Point, Point)] even for degenerate cases with equal coords
+  def rectangle_corners(other)
+    xs = [x, other.x].sort
+    ys = [y, other.y].sort
+    xs.product(ys).map { |x, y| self.class.new(x, y) }
+  end
 end
 
-class Floor
+VerticalEdge = Struct.new(:x, :y_range) do
+end
+
+class OrthogonalPolygon
+  attr_reader :points
+
+  # sorted by x
+  attr_reader :vertical_edges
+
   def self.from_file(filename)
     text = File.read(filename)
     points = text.lines.map do |line|
@@ -24,9 +39,46 @@ class Floor
   end
 
   def initialize(points)
+    raise "Too few points (#{points.size})" if points.size < 4
+
     @points = points
+
+    @vertical_edges = []
+    # cons are overlapping consecutive tuples (not slices)
+    (@points + [@points.first]).each_cons(2) do |a, b|
+      if a.x == b.x
+        y_range = Range.new(* [a.y, b.y].sort) # inclusive range
+        @vertical_edges << VerticalEdge.new(a.x, y_range)
+      else
+        raise "Edge not orthogonal: #{a.inspect}--#{b.inspect}" unless a.y == b.y
+      end
+    end
+
+    @vertical_edges.sort_by!(&:x)
   end
 
+  # corners and border does count as inside
+  def inside?(point)
+    # Cast a ray from `point` to the left
+    # and count the times it intersects the vertical edges
+    candidates = @vertical_edges.find_all { |e| e.y_range.cover?(point.y) }
+
+    # Array#bsearch and Array#bsearch_index note:
+    # In find-minimum mode which we use,
+    # it expects that theblock returns
+    # false for smaller indices, and
+    # true for indices greater or equal than the sought one
+
+    # greater_or_equal_edge_index
+    goei = candidates.bsearch_index { |e| e.x >= point.x }
+    return false if goei.nil?
+
+    equal = candidates[goei].x == point.x
+    goei.odd? || equal
+  end
+end
+
+class Floor < OrthogonalPolygon
   def max_red_area
     max = 0
 
@@ -41,10 +93,29 @@ class Floor
 
     max
   end
+
+  def max_red_area_within_green
+    max = 0
+
+    @points.each do |a|
+      @points.each do |b|
+        next if a >= b
+
+        corners = a.rectangle_corners(b)
+        next if corners.any? { |p| !inside?(p) }
+
+        area = a.rectangle_area(b)
+        max = area if max < area
+      end
+    end
+
+    max
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
   f = Floor.from_file(ARGV[0] || "input.txt")
-  
+
   puts "Maximal red rectangle: #{f.max_red_area}"
+  puts "Maximal red rectangle within green area: #{f.max_red_area_within_green}"
 end
